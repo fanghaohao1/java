d6fdf124b1ea321a2a4e55ed47bdecead9149221
房皓皓 <fanghaohao123@qq.com>
2021-11-27 00:27:06 +0800

    TEST1/src/Test1.java

---
+++ Java作业/TEST1/src/Test1.java
@@ -0,0 +1,10 @@
+
+public class Test1 {
+	public static void main(String[] args) {
+		int lightspeed=300000;//定义光速变量：光一秒钟传播的距离为30万公里
+		int days=100;//100天
+		long distance=lightspeed*3600*24*days;
+		System.out.println("光"+days+"天传播距离为"+distance+"公里");
+	}
+
+}
